3f9cf5cd1b97067b47d22fbf0f8065ede4a5d6fe
Prabaharan <142410995+Prabaharan45@users.noreply.github.com>
2025-08-30 11:42:04 +0530

    Update fishes.js

--- src/data/fishes.js
+++ src/data/fishes.js
@@ -1,14 +1,14 @@
 const fishes = [
-  { name: "Goldfish - 15", prices: [100, 150, 200], image: require("../Assets/Goldfish.jpg") },
-  { name: "Betta - 20", prices: [120, 180, 250], image: require("../Assets/Bettafish.jpg") },
-  { name: "Guppy - 30", prices: [80, 120, 170], image: require("../Assets/Guppyfish.jpg") },
-  { name: "Oscar - 10", prices: [300, 450, 600], image: require("../Assets/Oscarfish.jpg") },
-  { name: "Arowana - 8", prices: [1000, 1500, 2000], image: require("../Assets/Arowanafish.jpg") },
-  { name: "FlowerHorn - 12", prices: [200, 300, 400], image: require("../Assets/Flowerhornfish.jpg") },
-  { name: "Angel - 25", prices: [150, 250, 350], image: require("../Assets/Angelfish.jpg") },
-  { name: "Gora - 14", prices: [400, 600, 800], image: require("../Assets/Gorafish.jpg") },
-  { name: "Molly - 18", prices: [90, 140, 200], image: require("../Assets/Mollyfish.jpg") },
-  { name: "Koi - 9", prices: [500, 800, 1200], image: require("../Assets/Koifish.jpg") },
+  { name: "Goldfish - 100", prices: [20,60,100], image: require("../Assets/Goldfish.jpg") },
+  { name: "Betta - 50", prices: [100,200,300], image: require("../Assets/Bettafish.jpg") },
+  { name: "Guppy - 150", prices: [50,80,100], image: require("../Assets/Guppyfish.jpg") },
+  { name: "Oscar - 30", prices: [150,300,600], image: require("../Assets/Oscarfish.jpg") },
+  { name: "Arowana -15 ", prices: [1100, 2000,3000], image: require("../Assets/Arowanafish.jpg") },
+  { name: "FlowerHorn -20", prices: [100,600,1200], image: require("../Assets/Flowerhornfish.jpg") },
+  { name: "Angel - 80", prices: [50,100,200], image: require("../Assets/Angelfish.jpg") },
+  { name: "Gora - 100", prices: [50,100,150], image: require("../Assets/Gorafish.jpg") },
+  { name: "Molly - 210", prices: [20,60,100], image: require("../Assets/Mollyfish.jpg") },
+  { name: "Koi - 100", prices: [40,80,120], image: require("../Assets/Koifish.jpg") },
 ];
 
 export default fishes;
